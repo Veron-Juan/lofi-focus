d43e561080d7d8fefea1220f88b443a36391b3d7
Juan Verón <juan_juve97@hotmail.com>
2023-06-08 11:07:05 -0300

    feat: home responsive

--- app/page.tsx
+++ app/page.tsx
@@ -17,17 +17,27 @@ import {
 export default function IndexPage() {
   return (
     <>
-      <section className="flex justify-center   max-w-7xl mx-auto mt-6 xs:flex-col xs:items-center xs:px-7 ">
-        <div className="flex max-w-[900px] flex-col  justify-start">
-          <h1 className=" lg:text-7xl font-extrabold leading-tight tracking-tighter md:text-4xl xs:text-3xl text-black dark:text-white">
-            Welcome to Lofi Focus
+      <section className="flex justify-center md:items-center  max-w-7xl mx-auto mt-16 flex-col items-center px-7   ">
+      <h1 className="text-3xl text-center md:text-5xl lg:text-7xl font-extrabold leading-tight tracking-tighter   text-black dark:text-white ">
+          Your space for relaxation and concentration
           </h1>
-          <h2 className=" my-6  leading-tight tracking-tighter md:text-4xl xs:text-2xl  text-black dark:text-white">
+
+
+        
+
+
+
+        <div className="md:flex items-center  ">
+
+          
+          
+        <div className="flex md:flex-1 md:mb-[40px] max-w-[900px] flex-col  ">
+          
+          {/* <h2 className=" my-3  leading-tight tracking-tighter md:text-4xl text-2xl  text-black dark:text-white">
             Your space for relaxation and concentration.{" "}
-          </h2>
-          <p className="text-xl xs:text-[16px] max-w-[700px] text-muted-foreground">
-            Lofi music is a style of music popular for its soothing, soft and
-            nostalgic sound, ideal for studying, working and relaxing. On our
+          </h2> */}
+          <p className=" md:text-lg text-[16px] max-w-[600px] text-muted-foreground">
+            On our
             website, you can find a wide variety of lofi music, share your
             favorite songs with other users and discover new artists and songs
             in the community.
@@ -47,35 +57,22 @@ export default function IndexPage() {
             </Link>
           </div>
         </div>
-        <div className="flex-1 flex justify-end mt-4">
+        <div className="hidden md:block justify-start   mt-4">
           <img
-            className="max-w-md h-auto xs:w-[220px] xs:h-[220px]"
+            className=" w-[220px] h-[220px]  md:w-[300px] md:h-[300px] lg:w-[350px] lg:h-[350px] "
             src="/hero.png"
             alt="hero"
           />
         </div>
 
-        {/* <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
-      </div> */}
+        </div>
 
-<div className="flex  gap-6   mt-12  ">
-        <Card className="w-[300px] xs:w-[90px] xs:h-[90px] h-40 flex items-center justify-center gap-3  hover:bg-neutral-200/90 dark:hover:bg-inherit dark:hover:opacity-60 ">
+        
+
+
+
+<div className="flex  gap-6   mt-12 md:mt-6 ">
+        <Card className=" w-[90px] h-[90px] md:w-[120px] md:h-[120px] flex items-center justify-center gap-3  hover:bg-neutral-200/90 dark:hover:bg-inherit dark:hover:opacity-60 ">
           <CardHeader>
             <CardTitle className="flex items-center gap-2 xs:flex-col xs:text-xs">
               <Music strokeWidth={2.5} />
@@ -86,7 +83,7 @@ export default function IndexPage() {
         </CardDescription> */}
           </CardHeader>
         </Card>
-        <Card className=" w-[300px] xs:w-[90px] xs:h-[90px]  h-40 flex items-center justify-center gap-3  hover:bg-neutral-200/90 dark:hover:bg-inherit dark:hover:opacity-60">
+        <Card className=" w-[90px] h-[90px] md:w-[120px] md:h-[120px] flex items-center justify-center gap-3  hover:bg-neutral-200/90 dark:hover:bg-inherit dark:hover:opacity-60">
           <CardHeader>
             <CardTitle className="flex items-center gap-2 xs:flex-col xs:text-xs ">
               <TimerReset strokeWidth={2.5} />
@@ -97,7 +94,7 @@ export default function IndexPage() {
         </CardDescription> */}
           </CardHeader>
         </Card>
-        <Card className="w-[300px] xs:w-[90px] xs:h-[90px]   h-40 flex items-center justify-center gap-3  hover:bg-neutral-200/90 dark:hover:bg-inherit dark:hover:opacity-60">
+        <Card className="w-[90px] h-[90px] md:w-[120px] md:h-[120px] flex items-center justify-center gap-3  hover:bg-neutral-200/90 dark:hover:bg-inherit dark:hover:opacity-60">
           <CardHeader>
             <CardTitle className="flex items-center gap-2 xs:flex-col xs:text-xs">
               <Users strokeWidth={2.5} />
@@ -109,13 +106,13 @@ export default function IndexPage() {
           </CardHeader>
         </Card>
       </div>
-
+      
 
       </section>
       
       {/* <div className="max-w-[1000px]  flex justify-between items-start mt-2 gap-24 xs:gap-7 xs:flex-col xs:items-center" > */}
 
-      <div className=" flex flex-col  mx-auto items-center mt-20 gap-6">
+      <div className=" flex flex-col  items-center md:justify-center md:flex-row md:mb-40 md:px-6 mt-20  gap-6">
         <Image
           src="/sculpturemusicbg.png"
           alt="image"
@@ -124,15 +121,15 @@ export default function IndexPage() {
           height={380}
         />
         <div className="flex flex-col gap-5 max-w-[480px]  xs:my-1  xs:text-center">
-          <h3 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl  ">
+          <h3 className="scroll-m-20 text-4xl font-extrabold tracking-tight md:text-start md:ml-4 lg:text-5xl  ">
             Lofi Music
           </h3>
-          <p className="text-justify px-7">
+          <p className="text-justify px-7 md:px-4">
             Edit photos quickly and accurately without all the effort. PhotoRoom
             makes it easy to keep the focus on the foreground, plus it's 2x more
             accurate than other apps.
           </p>
-          <div className="text-[15px]">
+          <div className="text-[15px] md:text-start md:ml-3">
             <Link
               href="/lofi"
               className={buttonVariants({ variant: "black", size: "default" })}
@@ -143,18 +140,18 @@ export default function IndexPage() {
         </div>
       </div>
 
-      <div className="flex  mt-20 gap-7  flex-col-reverse items-center  ">
+      <div className="flex  mt-20 gap-7  flex-col-reverse items-center md:justify-center md:flex-row md:mb-40   md:px-6  ">
         <div className="flex flex-col gap-5 max-w-[480px]  xs:my-1  xs:text-center">
-          <h3 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl ">
+          <h3 className="scroll-m-20 text-4xl font-extrabold tracking-tight md:text-5xl ">
             Pomodoro Tecnique
           </h3>
 
-          <p className="text-justify px-7">
+          <p className="text-justify px-7 md:px-4">
             Edit photos quickly and accurately without all the effort. PhotoRoom
             makes it easy to keep the focus on the foreground, plus it's 2x more
             accurate than other apps.
           </p>
-          <div className="text-[15px]">
+          <div className="text-[15px] md:text-start md:ml-3">
             <Link
               href="/lofi"
               className={buttonVariants({ variant: "black", size: "default" })}
@@ -172,7 +169,7 @@ export default function IndexPage() {
         />
       </div>
 
-      <div className="flex flex-col  mx-auto items-center mt-20 gap-6">
+      <div className=" flex flex-col  items-center md:justify-center md:flex-row  md:px-6 md:mb-40  mt-20 gap-6">
         <Image
           src="/community.png"
           alt="image"
@@ -181,15 +178,15 @@ export default function IndexPage() {
           height={380}
         />
         <div className="flex flex-col gap-5 max-w-[480px]  xs:my-1  xs:text-center">
-          <h3 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl  ">
+          <h3 className="scroll-m-20 text-4xl font-extrabold tracking-tight md:text-start md:ml-4 lg:text-5xl  ">
             Community
           </h3>
-          <p className="text-justify px-7">
+          <p className="text-justify px-7 md:px-4">
             Edit photos quickly and accurately without all the effort. PhotoRoom
             makes it easy to keep the focus on the foreground, plus it's 2x more
             accurate than other apps.
           </p>
-          <div className="text-[15px]">
+          <div className="text-[15px] md:text-start md:ml-3">
             <Link
               href="/lofi"
               className={buttonVariants({ variant: "black", size: "default" })}
