2fc48632e0f23fe400890a7a762b67ae993cca6f
Juan Verón <juan_juve97@hotmail.com>
2023-06-27 21:39:36 -0300

    delete file from utils

--- utils/db.ts
+++ utils/db.ts
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const connect = async () => {
   try {
-    if (!process.env.MONGO) {
+    if (!process.env.MONGODB_URI) {
       throw new Error("MongoDB connection string is not defined");
     }
 
-    await mongoose.connect(process.env.MONGO);
+    await mongoose.connect(process.env.MONGODB_URI);
   } catch (error) {
     throw new Error("Connection failed!");
   }
